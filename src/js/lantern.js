958c71d11f24867ecce59e14a84de330aa9dd6d1
Brian Burns <bburns.km@gmail.com>
2016-11-20 12:39:24 -0600

    refactor: rename d->data, move files

--- src/js/lantern.js
+++ src/js/lantern.js
@@ -1,4 +1,24 @@
 
+// Lantern
+// Explore Zork map interactively with d3
+// --------------------------------------------------------------------------------
+
+// For reference, see
+// http://bl.ocks.org/mbostock/3750558
+
+
+// The data structures read in look like this - 
+// var rooms = [
+//     {"key": "WHOUS", "name": "West of House", "desc": "This is an open field west of a white house, with a boarded front door."},
+//     {"key": "ATTIC", "name": "Attic", "desc": "This is the attic.  The only exit is stairs that lead down."}];
+// var exits = [
+//     {"source": "WHOUS", "dir": "NORTH", "target": "NHOUS"},
+//     {"source": "WHOUS", "dir": "EAST", "target": "The door is locked, and there is evidently no key."}];
+
+
+// this code needs major cleanup - very hard to read/understand
+// several undefined globals
+
 
 // globals from other files
 var d3;
@@ -8,13 +28,14 @@ var g;
 
 
 //--------------------------------------------------------------------------------
-// * Graph module
+// * Graph
 //--------------------------------------------------------------------------------
 
 //> this module defines a Graph g which...
 
-
 (function () {
+    
+    // initialize the graph object
 
     var distance = 90; // fixed distance between nodes
     var charge = -5000; // attractive/repulsive force
@@ -40,25 +61,23 @@ var g;
     // since we won't be duplicating links very much
     // var linkkeys = new Set();
 
+    
     // create svg canvas
     var svg = d3.select("#map").append("svg");
     
-    // scale contents with browser window resizing.
-    // svg.attr("viewBox", "0 0 " + width + " " + height ).attr("preserveAspectRatio", "xMidYMid meet");
-    // svg.call(d3.behavior.zoom().on("zoom", redraw));
     // note that the size of the svg rectangle is INDEPENDENT of the svg size -
     // just want it to match it somehow
+
+    // set size of svg
     var size = getWindowSize();
     size[1]-=100; //> arbitrary
+    svg.append("rect").attr("width", size[0]).attr("height", size[1]);
+
     //> can you read the width and height of the map div instead of whole window?
     // nowork
     // var map = document.getElementById('map');
     // var size = [map.offsetWidth, map.offsetHeight];
     // pj(size);
-
-    // svg.append("rect").attr("width", width).attr("height", height);
-    svg.append("rect").attr("width", size[0]).attr("height", size[1]);
-
 
     // // set force size to svg size
     // // var width2 = svg.offsetWidth;
@@ -72,8 +91,8 @@ var g;
     // p(svg[0]);
     // p(document);
     // // svg = document.getElementsByClassName('svg')[0];
-    var e = document.getElementsByTagName('svg')[0];
-    p(e.clientWidth);
+    // var e = document.getElementsByTagName('svg')[0];
+    // p(e.clientWidth);
     // // svg.load(function() {
     // // p(svg.localName);
     // // p(svg.border);
@@ -85,6 +104,10 @@ var g;
     // // p(svg.getBoundingClientRect);
     // // });
 
+    // scale contents with browser window resizing.
+    // svg.attr("viewBox", "0 0 " + width + " " + height ).attr("preserveAspectRatio", "xMidYMid meet");
+    // svg.call(d3.behavior.zoom().on("zoom", redraw));
+
 
     var force = d3.layout.force()
         .size(size)
@@ -93,6 +116,8 @@ var g;
         .gravity(gravity)
         .on("tick", tick);
     
+
+    // this function is called on each time tick to animate the graph
     function tick() {
         nodeData
             .attr("transform", function(d) {return "translate(" + d.x + "," + d.y + ")";});
@@ -105,39 +130,61 @@ var g;
             .attr("y2", function(d) { return d.target.y; });
     };
 
-
+    
     // add/update/delete svg elements for current nodes and links
-    var updateSvg = function () {
+    function updateSvg() {
+        
+        // links
+        
+        // select all line elements with class 'link'
+        linkData = svg.selectAll("line.link")
+            .data(force.links(), function(d) { return d.source.key + "-" + d.target.key; }); // eg "whous-shous"
+        linkLine = linkData.enter()
+            .append("line")
+            .attr("class", "link"); // add new elements
+        linkData.exit()
+            .remove(); // remove old elements
+
+        
         // nodes
+        
         // groups with circles or rectangles and labels
         // select all g elements with class 'node'
-        nodeData = svg.selectAll("g.node").data(force.nodes(), function(d) { return d.key;});
-        nodeGroup = nodeData.enter().append("g").attr("class", "node"); // add new elements
+        nodeData = svg.selectAll("g.node")
+            .data(force.nodes(), function(d) { return d.key;});
+        nodeGroup = nodeData.enter()
+            .append("g")
+            .attr("class", "node"); // add new elements
         nodeGroup.call(force.drag); // make nodes draggable
         nodeGroup.on("click", onclick); // callback fn for user to set
-        nodeGroup.append("svg:title").text(function(d) { return d.desc || "(No description)"; }); // tooltips
-        nodeData.exit().remove(); // remove old elements
+        nodeGroup.append("svg:title")
+            .text(function(d) { return d.desc || "(No description)"; }); // tooltips
+        nodeData.exit()
+            .remove(); // remove old elements
 
         if (shape=="circle")
-            nodeCircle = nodeGroup.append("circle").attr("r", radius);
+            nodeCircle = nodeGroup.append("circle")
+            .attr("r", radius);
+        
         if (shape=="rect")
-            nodeRect = nodeGroup.append("rect").attr("width", rect_width).attr("height", rect_height);
+            nodeRect = nodeGroup.append("rect")
+            .attr("width", rect_width)
+            .attr("height", rect_height);
 
-        var nodeLabel = nodeGroup.append("text").attr("class", "nodetext").attr("x", labelx).attr("y", labely).text(function(d) {return d.name;});
-        // .attr("dx", radius + 3)
-        // .attr("dy", ".35em")
-
-        // links
-        // select all line elements with class 'link'
-        linkData = svg.selectAll("line.link").data(force.links(), function(d) { return d.source.key + "-" + d.target.key; }); // eg "whous-shous"
-        linkLine = linkData.enter().append("line").attr("class", "link"); // add new elements
-        linkData.exit().remove(); // remove old elements
+        var nodeLabel = nodeGroup.append("text")
+            .attr("class", "nodetext")
+            .attr("x", labelx)
+            .attr("y", labely)
+            // .attr("dx", radius + 3)
+            // .attr("dy", ".35em")
+            .text(function(d) {return d.name;});
 
         // restart the force layout
         force.start();
     };
 
-    // export a global variable / module / namespace
+    
+    // export a global variable/module/namespace
     g = {
 
         // add a node to the graph. a node is just an object with a .key property.
@@ -173,68 +220,37 @@ var g;
 
 
 //--------------------------------------------------------------------------------
-// * Data modules
+// * Data
 //--------------------------------------------------------------------------------
 
-//> ?
+//> what is this?
 
-(function() {
-
-    var rooms = [
-        {"key": "WHOUS", "name": "West of House", "desc": "This is an open field west of a white house, with a boarded front door."},
-        {"key": "NHOUS", "name": "North of House", "desc": "You are facing the north side of a white house.  There is no door here."},
-        {"key": "SHOUS", "name": "South of House", "desc": "You are facing the south side of a white house. There is no door here."},
-        {"key": "EHOUS", "name": "Behind House", "desc": ""},
-        {"key": "KITCH", "name": "Kitchen", "desc": ""},
-        {"key": "ATTIC", "name": "Attic", "desc": "This is the attic.  The only exit is stairs that lead down."},
-        {"key": "LROOM", "name": "Living Room", "desc": ""}];
-
-    var exits = [
-        {"source":"WHOUS", "dir":"NORTH", "target":"NHOUS"},
-        {"source":"WHOUS", "dir":"SOUTH", "target":"SHOUS"},
-        {"source":"WHOUS", "dir":"WEST", "target":"FORE1"},
-        {"source":"WHOUS", "dir":"EAST", "target":"The door is locked, and there is evidently no key."},
-        {"source":"NHOUS", "dir":"WEST", "target":"WHOUS"},
-        {"source":"NHOUS", "dir":"EAST", "target":"EHOUS"},
-        {"source":"NHOUS", "dir":"NORTH", "target":"FORE3"},
-        {"source":"NHOUS", "dir":"SOUTH", "target":"The windows are all barred."},
-        {"source":"SHOUS", "dir":"WEST", "target":"WHOUS"},
-        {"source":"SHOUS", "dir":"EAST", "target":"EHOUS"},
-        {"source":"SHOUS", "dir":"SOUTH", "target":"FORE2"},
-        {"source":"SHOUS", "dir":"NORTH", "target":"The windows are all barred."}];
-
-    d = {
-        init: function(ignore, fn) {
-            // use hardcoded rooms and exits
-            fn();
-        },
-        initjson: function(filename, fn) {
-            d3.json(filename, function(error, json) {
-                if (error) return console.warn(error);
-                rooms = json['rooms'];
-                exits = json['exits'];
-                return fn();
-            });
-        },
-        // find the given room object.
-        // linear search in lieu of a hash for now
-        // getroom: function(roomkey) {
-        //     var room = findObject(rooms, 'key', roomkey);
-        //     return room;
-        // },
-        get: function(roomkey, fn) {
-            var room = findObject(rooms, 'key', roomkey);
-            fn(room);
-        },
-        // find all of the exits from the given room - each exit object looks like this
-        // {source:'whous', target:'shous', dir:'east'}
-        // return in a list
-        getexits: function(roomkey) {
-            return exits.filter(function (exit) {return exit.source===roomkey;});
-        }
-    };
-
-})();
+var data = {
+    init: function(filename, fn) {
+        d3.json(filename, function(error, json) {
+            if (error) return console.warn(error);
+            rooms = json['rooms'];
+            exits = json['exits'];
+            return fn();
+        });
+    },
+    // find the given room object.
+    // linear search in lieu of a hash for now
+    // getroom: function(roomkey) {
+    //     var room = findObject(rooms, 'key', roomkey);
+    //     return room;
+    // },
+    get: function(roomkey, fn) {
+        var room = findObject(rooms, 'key', roomkey);
+        fn(room);
+    },
+    // find all of the exits from the given room - each exit object looks like this
+    // {source:'whous', target:'shous', dir:'east'}
+    // return in a list
+    getexits: function(roomkey) {
+        return exits.filter(function (exit) {return exit.source===roomkey;});
+    }
+};
 
 
 
@@ -245,13 +261,13 @@ var g;
 // click on room callback
 function onclick(d,i) { addRoomExits(d); }
 
-// given a room d, find all its exits.
+// given a room, find all its exits.
 // then add those rooms and the links to them called by onclick handler.
 function addRoomExits(room) {
 
     // find all exits from this room
     var sourcekey = room.key;
-    var roomexits = d.getexits(sourcekey);
+    var roomexits = data.getexits(sourcekey);
 
     // add (possibly) new rooms and links
     roomexits.map(function (exit) {
@@ -263,7 +279,7 @@ function addRoomExits(room) {
         // g.addnode(target);
         // g.addlink(sourcekey, targetkey, dir);
 
-        d.get(targetkey, function (target) {
+        data.get(targetkey, function (target) {
             if (target) {
                 g.addnode(target);
                 g.addlink(sourcekey, targetkey, dir);
@@ -274,28 +290,27 @@ function addRoomExits(room) {
 
 
 //--------------------------------------------------------------------------------
-
-// * start
+// * Start
+//--------------------------------------------------------------------------------
 
 // nowork - why? can only do child dirs?
 //// var filename = '../../data/json/zork_rooms.json';
 //var filename = '../../data/json/zork_rooms_small.json';
 
 // var filename = 'zork_rooms.json';
-var filename = 'zork_rooms_small.json';
+var filename = 'data/json/zork_rooms_small.json';
 var startkey = 'WHOUS';
 
 
 // arrays
-// d.init(null, function() {
-d.initjson(filename, function() {
-    d.get(startkey, function(room) {
-       g.addnode(room);
+data.init(filename, function() {
+    data.get(startkey, function(room) {
+        g.addnode(room);
     });
 });
 
 
-// var whous = d.getroom(startkey);
+// var whous = data.getroom(startkey);
 // g.addnode(whous);
 
 // g.addnode(rooms[0]);
